25bac2d49ba8d8a517939d12460fd0effa6776f7
Anton Gushcha <ncrashed@protonmail.com>
2021-03-17 17:44:07 +0300

    Refactor. Factor out filter generation and storage to filter module

---
+++ src/filter.rs
@@ -0,0 +1,50 @@
+use bitcoin_utxo::cache::utxo::*;
+use bitcoin::{Block, BlockHash, OutPoint, Script};
+use bitcoin::util::bip158;
+use ergvein_filters::btc::ErgveinFilter;
+use rocksdb::{WriteBatch, DB, WriteOptions};
+use std::collections::HashMap;
+use std::sync::Arc;
+use super::utxo::FilterCoin;
+use tokio::time::Duration;
+
+pub async fn generate_filter(
+    db: Arc<DB>,
+    cache: Arc<UtxoCache<FilterCoin>>,
+    h: u32,
+    block: Block,
+) -> ErgveinFilter {
+    let mut hashmap = HashMap::<OutPoint, Script>::new();
+    for tx in &block.txdata {
+        if !tx.is_coin_base() {
+            for i in &tx.input {
+                let coin = wait_utxo(
+                    db.clone(),
+                    cache.clone(),
+                    &i.previous_output,
+                    h,
+                    Duration::from_millis(100),
+                )
+                .await;
+                hashmap.insert(i.previous_output, coin.script);
+            }
+        }
+    }
+    ErgveinFilter::new_script_filter(&block, |out| {
+        hashmap
+            .get(out)
+            .map_or(Err(bip158::Error::UtxoMissing(*out)), |s| Ok(s.clone()))
+    })
+    .unwrap()
+}
+
+pub fn store_filter(db: Arc<DB>, hash: &BlockHash, filter: ErgveinFilter) {
+    let cf = db.cf_handle("filters").unwrap();
+    let mut batch = WriteBatch::default();
+    batch.put_cf(cf, hash, filter.content);
+
+    let mut write_options = WriteOptions::default();
+    write_options.set_sync(false);
+    write_options.disable_wal(true);
+    db.write_opt(batch, &write_options).unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,17 @@ extern crate bitcoin_utxo;
 extern crate ergvein_filters;
 
 pub mod utxo;
+pub mod filter;
 
 use crate::utxo::FilterCoin;
+use crate::filter::*;
 
 use futures::future::{AbortHandle, Abortable, Aborted};
 use futures::pin_mut;
 use futures::stream;
 use futures::SinkExt;
 
-use rocksdb::{WriteBatch, DB, WriteOptions};
-
 use hex;
-use std::collections::HashMap;
 use std::error::Error;
 use std::net::SocketAddr;
 use std::sync::Arc;
@@ -23,9 +22,6 @@ use std::{env, process};
 use tokio::time::Duration;
 
 use bitcoin::network::constants;
-use bitcoin::util::bip158;
-use ergvein_filters::btc::ErgveinFilter;
-use bitcoin::{Block, BlockHash, OutPoint, Script};
 
 use bitcoin_utxo::cache::utxo::*;
 use bitcoin_utxo::connection::connect;
@@ -117,45 +113,4 @@ async fn main() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
-}
-
-async fn generate_filter(
-    db: Arc<DB>,
-    cache: Arc<UtxoCache<FilterCoin>>,
-    h: u32,
-    block: Block,
-) -> ErgveinFilter {
-    let mut hashmap = HashMap::<OutPoint, Script>::new();
-    for tx in &block.txdata {
-        if !tx.is_coin_base() {
-            for i in &tx.input {
-                let coin = wait_utxo(
-                    db.clone(),
-                    cache.clone(),
-                    &i.previous_output,
-                    h,
-                    Duration::from_millis(100),
-                )
-                .await;
-                hashmap.insert(i.previous_output, coin.script);
-            }
-        }
-    }
-    ErgveinFilter::new_script_filter(&block, |out| {
-        hashmap
-            .get(out)
-            .map_or(Err(bip158::Error::UtxoMissing(*out)), |s| Ok(s.clone()))
-    })
-    .unwrap()
-}
-
-fn store_filter(db: Arc<DB>, hash: &BlockHash, filter: ErgveinFilter) {
-    let cf = db.cf_handle("filters").unwrap();
-    let mut batch = WriteBatch::default();
-    batch.put_cf(cf, hash, filter.content);
-
-    let mut write_options = WriteOptions::default();
-    write_options.set_sync(false);
-    write_options.disable_wal(true);
-    db.write_opt(batch, &write_options).unwrap();
 }
