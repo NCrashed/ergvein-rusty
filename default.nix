c2d424b0fcb19a051d8d9384336407c87262bb34
Anton Gushcha <existonce@protonmail.com>
2021-04-13 20:14:31 +0300

    Use fixed rust toolchain

--- default.nix
+++ default.nix
@@ -6,10 +6,11 @@ let
       [
         nixpkgs-mozilla
         (self: super:
-            {
-              rustc = self.latest.rustChannels.nightly.rust;
-              cargo = self.latest.rustChannels.nightly.rust;
-            }
+          let chan = self.rustChannelOf { date = "2021-03-01"; channel = "nightly"; };
+          in {
+            rustc = chan.rust;
+            cargo = chan.rust;
+          }
         )
       ];
   };
