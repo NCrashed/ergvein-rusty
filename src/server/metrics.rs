d8342d334b1afe05639dc3177b265e4873ed9f88
Anton Gushcha <ncrashed@protonmail.com>
2021-08-04 16:45:22 +0300

    Fix async endpoint

--- src/server/metrics.rs
+++ src/server/metrics.rs
@@ -4,7 +4,7 @@ use prometheus::{self, Encoder, IntCounter, IntGauge, TextEncoder};
 use rocksdb::DB;
 use std::net::SocketAddr;
 use std::sync::Arc;
-use warp::Filter;
+use warp::{Filter, Rejection, path};
 use crate::server::block_explorer::*;
 use futures::executor;
 
@@ -46,23 +46,28 @@ pub async fn serve_metrics(addr: SocketAddr, db: Arc<DB>, db_path: String) {
     BTC_SCAN_GAUGE.set(get_filters_height(&db) as i64);
     SPACE_GAUGE.set(fs2::available_space(db_path.clone()).unwrap_or(0) as i64);
     
-    let db = db.clone();
-    let metrics = warp::path::param().and_then(async move || {
-        let btc_actual_height = ask_btc_actual_height().await;
-        match btc_actual_height {
-          Ok(height) => BTC_BLOCK_EXPLORER_HEIGHT.set (height),
-          _ => BTC_BLOCK_EXPLORER_HEIGHT.set(0)
+    
+    let metrics = path!("/").and_then(move || {
+        let db = db.clone();
+        let db_path = db_path.clone();
+        async move {
+            let btc_actual_height = ask_btc_actual_height().await;
+            match btc_actual_height {
+              Ok(height) => BTC_BLOCK_EXPLORER_HEIGHT.set (height),
+              _ => BTC_BLOCK_EXPLORER_HEIGHT.set(0)
+            }
+            BTC_HEIGHT_GAUGE.set(get_chain_height(&db) as i64);
+            BTC_SCAN_GAUGE.set(get_filters_height(&db) as i64);
+            SPACE_GAUGE.set(fs2::available_space(&db_path).unwrap_or(0) as i64);
+    
+            let mut buffer = Vec::new();
+            let metric_families = prometheus::gather();
+            TextEncoder::new()
+                .encode(&metric_families, &mut buffer)
+                .unwrap();
+                Result::<_, Rejection>::Ok(String::from_utf8(buffer).unwrap())
         }
-        BTC_HEIGHT_GAUGE.set(get_chain_height(&db) as i64);
-        BTC_SCAN_GAUGE.set(get_filters_height(&db) as i64);
-        SPACE_GAUGE.set(fs2::available_space(&db_path).unwrap_or(0) as i64);
-
-        let mut buffer = Vec::new();
-        let metric_families = prometheus::gather();
-        TextEncoder::new()
-            .encode(&metric_families, &mut buffer)
-            .unwrap();
-        String::from_utf8(buffer).unwrap()
+ 
     });
     let routes = warp::get().and(metrics);
 
