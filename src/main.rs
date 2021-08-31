297ea1f8e802031c370106090d1d3d1a506d8aaf
Aminion <aminion@protonmail.com>
2021-08-31 21:47:25 +0300

    Finish moving config.

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ extern crate bitcoin;
 extern crate bitcoin_utxo;
 extern crate byteorder;
 extern crate bytes;
-extern crate clap;
-extern crate ergvein_filters;
 extern crate fs2;
 extern crate reqwest;
 extern crate serde;
@@ -39,10 +37,8 @@ use mempool_filters::filtertree::FilterTree;
 use mempool_filters::txtree::TxTree;
 use mempool_filters::worker::mempool_worker;
 use std::error::Error;
-use std::net::{IpAddr, SocketAddr};
 use std::sync::Arc;
 use std::sync::Mutex;
-use std::{env, process};
 use tokio::time::Duration;
 
 use bitcoin::network::constants;
@@ -51,142 +47,28 @@ use bitcoin_utxo::cache::utxo::*;
 use bitcoin_utxo::connection::connect;
 use bitcoin_utxo::storage::init_storage;
 use bitcoin_utxo::sync::headers::sync_headers;
-use bitcoin_utxo::sync::utxo::DEF_BLOCK_BATCH;
-
-use clap::{crate_version, value_t, App, Arg};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let fork_depth_def = &UTXO_FORK_MAX_DEPTH.to_string();
-    let cache_size_def = &UTXO_CACHE_MAX_COINS.to_string();
-    let flush_period_def = &UTXO_FLUSH_PERIOD.to_string();
-    let block_batch_def = &DEF_BLOCK_BATCH.to_string();
-    let matches = App::new("ergvein-rusty")
-        .about("P2P node to support ergvein mobile wallet")
-        .version(crate_version!())
-        .arg(
-            Arg::with_name("host")
-                .short("n")
-                .long("host")
-                .value_name("HOST")
-                .help("Listen network interface")
-                .default_value("0.0.0.0")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("port")
-                .short("p")
-                .long("port")
-                .value_name("PORT")
-                .help("Listen port")
-                .default_value("8667")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("bitcoin")
-                .short("b")
-                .long("bitcoin")
-                .value_name("BITCOIN_NODE")
-                .help("Host and port for bitcoin node to use")
-                .default_value("127.0.0.1:8333")
-                .multiple(true)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("data")
-                .short("d")
-                .long("data")
-                .value_name("DATABASE_PATH")
-                .help("Directory where to store application state")
-                .default_value("./ergvein_rusty_db")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("fork-depth")
-                .long("fork-depth")
-                .value_name("BLOCKS_AMOUNT")
-                .help("Maximum reorganizatrion depth in blockchain")
-                .default_value(&fork_depth_def)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("max-cache")
-                .long("max-cache")
-                .value_name("COINS_AMOUNT")
-                .help("Maximum size of cache in coins amount, limits memory for cache")
-                .default_value(&cache_size_def)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("flush-period")
-                .long("flush-period")
-                .value_name("BLOCKS_AMOUNT")
-                .help("Flush cache to disk every given amount of blocks")
-                .default_value(&flush_period_def)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("block-batch")
-                .long("block-batch")
-                .value_name("BLOCKS_AMOUNT")
-                .help("Amount of blocks to process in parallel while syncing")
-                .default_value(&block_batch_def)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("metrics-host")
-                .long("metrics-host")
-                .value_name("METRICS_HOST")
-                .help("Listen network interface for Prometheus metrics")
-                .default_value("0.0.0.0")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("metrics-port")
-                .long("metrics-port")
-                .value_name("METRICS_PORT")
-                .help("Listen port")
-                .default_value("9667")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("mempool-period")
-                .long("mempool-period")
-                .value_name("MEMPOOL_PERIOD")
-                .help("How often to rebuild mempool filters. Integer seconds")
-                .default_value("60")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("mempool-timeout")
-                .long("mempool-timeout")
-                .value_name("MEMPOOL_TIMEOUT")
-                .help("Filter timeout. After it, consider the attempt failed and yield all mutexes")
-                .default_value("10")
-                .takes_value(true),
-        )
-        .get_matches();
+    match app_config() {
+      Some(cfg) => {
+        println!("Opening database {:?}", &cfg.db_name);
 
-    let str_address = matches.value_of("bitcoin").unwrap();
+    let db = Arc::new(init_storage(&cfg.db_name, vec!["filters"])?);
 
-    let address: SocketAddr = str_address.parse().unwrap_or_else(|error| {
-        eprintln!("Error parsing address: {:?}", error);
-        process::exit(1);
-    });
-
-    let dbname = matches.value_of("data").unwrap();
-    println!("Opening database {:?}", dbname);
-    let db = Arc::new(init_storage(dbname, vec!["filters"])?);
     println!("Creating cache");
+
     let cache = Arc::new(new_cache::<FilterCoin>());
     let fee_cache = Arc::new(Mutex::new(FeesCache::default()));
     let rates_cache = Arc::new(new_rates_cache());
+
     tokio::spawn({
         let fee_cache = fee_cache.clone();
         async move {
             fees_requester(fee_cache).await;
         }
     });
+
     tokio::spawn({
         let rates_cache = rates_cache.clone();
         async move {
@@ -194,11 +76,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
         }
     });
 
-    let listen_addr = format!(
-        "{}:{}",
-        matches.value_of("host").unwrap(),
-        value_t!(matches, "port", u32).unwrap()
-    );
+    let listen_addr = format!("{}:{}", cfg.host, cfg.port);
+
     tokio::spawn({
         let db = db.clone();
         let fee_cache = fee_cache.clone();
@@ -210,23 +89,23 @@ async fn main() -> Result<(), Box<dyn Error>> {
         }
     });
 
-    let metrics_addr = SocketAddr::new(
-        value_t!(matches, "metrics-host", IpAddr).unwrap(),
-        value_t!(matches, "metrics-port", u16).unwrap(),
-    );
     tokio::spawn({
         let db = db.clone();
-        let dbname = dbname.to_string();
+        let db_name = cfg.db_name.clone();
+        let m_addr = cfg.metrics_addr.clone();
         async move {
-            println!("Start serving metrics at {}", metrics_addr);
-            serve_metrics(metrics_addr, db.clone(), dbname).await;
+            println!("Start serving metrics at {}", m_addr);
+            serve_metrics(m_addr, db, db_name).await;
         }
     });
 
     loop {
         let (headers_future, headers_stream, headers_sink) = sync_headers(db.clone()).await;
+
         pin_mut!(headers_sink);
+
         let (abort_headers_handle, abort_headers_registration) = AbortHandle::new_pair();
+
         tokio::spawn(async move {
             if let Err(Aborted) = Abortable::new(headers_future, abort_headers_registration).await {
                 eprintln!("Headers task was aborted!")
@@ -237,20 +116,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
             sync_filters(
                 db.clone(),
                 cache.clone(),
-                value_t!(matches, "fork-depth", u32).unwrap(),
-                value_t!(matches, "max-cache", usize).unwrap(),
-                value_t!(matches, "flush-period", u32).unwrap(),
-                value_t!(matches, "block-batch", usize).unwrap(),
+                cfg.fork_depth,
+                cfg.max_cache,
+                cfg.flush_period,
+                cfg.block_batch,
             )
             .await;
 
         let txtree = Arc::new(TxTree::new());
         let ftree = Arc::new(FilterTree::new());
         let full_filter = Arc::new(tokio::sync::RwLock::new(None));
-        let mempool_period =
-            Duration::from_secs(value_t!(matches, "mempool-period", u64).unwrap_or(60));
-        let mempool_timeout =
-            Duration::from_secs(value_t!(matches, "mempool-timeout", u64).unwrap_or(60));
         let (mempool_future, filters_sink, filters_stream) = mempool_worker(
             txtree,
             ftree,
@@ -259,8 +134,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
             cache.clone(),
             sync_mutex.clone(),
             coin_script,
-            mempool_period,
-            mempool_timeout,
+            cfg.mempool_period,
+            cfg.mempool_timeout,
         )
         .await;
 
@@ -280,7 +155,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         println!("Connecting to node");
         let res = Abortable::new(
             connect(
-                &address,
+                &cfg.node_address,
                 constants::Network::Bitcoin,
                 "rust-client".to_string(),
                 0,
@@ -310,7 +185,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 }
             },
         }
+    } 
+      }
+      None => {
+        println!("Error in app parameters");
+      }
     }
-
     Ok(())
 }
--- src/options.rs
+++ src/options.rs
@@ -1,17 +1,31 @@
 use clap::{crate_version, value_t, App, Arg};
-use bitcoin::network::constants;
 use bitcoin_utxo::cache::utxo::*;
-use bitcoin_utxo::connection::connect;
-use bitcoin_utxo::storage::init_storage;
-use bitcoin_utxo::sync::headers::sync_headers;
 use bitcoin_utxo::sync::utxo::DEF_BLOCK_BATCH;
+use tokio::time::Duration;
+use std::option::*;
 
-pub fn matches () -> App<'static, 'static> {
+use std::net::{IpAddr, SocketAddr};
+
+pub struct AppConfig {
+    pub node_address : SocketAddr,
+    pub db_name : String,
+    pub host : String,
+    pub port : u32,
+    pub metrics_addr : SocketAddr,
+    pub fork_depth : u32,
+    pub max_cache : usize,
+    pub flush_period : u32,
+    pub block_batch : usize,
+    pub mempool_period : Duration,
+    pub mempool_timeout : Duration
+}
+
+pub fn app_config () -> Option<AppConfig>  {
     let fork_depth_def = &UTXO_FORK_MAX_DEPTH.to_string();
     let cache_size_def = &UTXO_CACHE_MAX_COINS.to_string();
     let flush_period_def = &UTXO_FLUSH_PERIOD.to_string();
     let block_batch_def = &DEF_BLOCK_BATCH.to_string();
-    App::new("ergvein-rusty")
+    let app = App::new("ergvein-rusty")
         .about("P2P node to support ergvein mobile wallet")
         .version(crate_version!())
         .arg(
@@ -30,25 +44,6 @@ pub fn matches () -> App<'static, 'static> {
                 .value_name("PORT")
                 .help("Listen port")
                 .default_value("8667")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("bitcoin")
-                .short("b")
-                .long("bitcoin")
-                .value_name("BITCOIN_NODE")
-                .help("Host and port for bitcoin node to use")
-                .default_value("127.0.0.1:8333")
-                .multiple(true)
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("data")
-                .short("d")
-                .long("data")
-                .value_name("DATABASE_PATH")
-                .help("Directory where to store application state")
-                .default_value("./ergvein_rusty_db")
                 .takes_value(true),
         )
         .arg(
@@ -84,6 +79,25 @@ pub fn matches () -> App<'static, 'static> {
                 .takes_value(true),
         )
         .arg(
+            Arg::with_name("bitcoin")
+                .short("b")
+                .long("bitcoin")
+                .value_name("BITCOIN_NODE")
+                .help("Host and port for bitcoin node to use")
+                .default_value("127.0.0.1:8333")
+                .multiple(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("data")
+                .short("d")
+                .long("data")
+                .value_name("DATABASE_PATH")
+                .help("Directory where to store application state")
+                .default_value("./ergvein_rusty_db")
+                .takes_value(true),
+        )
+        .arg(
             Arg::with_name("metrics-host")
                 .long("metrics-host")
                 .value_name("METRICS_HOST")
@@ -114,5 +128,35 @@ pub fn matches () -> App<'static, 'static> {
                 .help("Filter timeout. After it, consider the attempt failed and yield all mutexes")
                 .default_value("10")
                 .takes_value(true),
-        )
+        );
+
+    let matches = app.get_matches();
+    let str_address = matches.value_of("bitcoin")?;
+    let address: SocketAddr = str_address.parse().ok()?;
+    let db_name = matches.value_of("data")?.to_string();
+    let host = matches.value_of("host")?.to_string();
+    let port = value_t!(matches, "port", u32).ok()?;
+    let metrics_host = value_t!(matches, "metrics-host", IpAddr).ok()?;
+    let metrics_port = value_t!(matches, "metrics-port", u16).ok()?;
+    let metrics_addr = SocketAddr::new(metrics_host, metrics_port);
+    let fork_depth = value_t!(matches, "fork-depth", u32).ok()?;
+    let max_cache = value_t!(matches, "max-cache", usize).ok()?;
+    let flush_period = value_t!(matches, "flush-period", u32).ok()?;
+    let block_batch = value_t!(matches, "block-batch", usize).ok()?;
+    let mempool_period = Duration::from_secs(value_t!(matches, "mempool-period", u64).unwrap_or(60));
+    let mempool_timeout = Duration::from_secs(value_t!(matches, "mempool-timeout", u64).unwrap_or(60));
+
+    Some(AppConfig {
+        node_address : address,
+        db_name : db_name, 
+        host : host,
+        port : port,
+        metrics_addr : metrics_addr,
+        fork_depth : fork_depth,
+        max_cache : max_cache,
+        flush_period : flush_period,
+        block_batch : block_batch,
+        mempool_period : mempool_period,
+        mempool_timeout : mempool_timeout
+    })
 }
